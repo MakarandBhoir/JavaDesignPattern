f98c5404e7042816e685f87a0c4711a335a36466
MakarandBhoir <makarandbhoir@synergetics-india.com>
2017-06-16 14:51:30 +0530

    Add files via upload

---
+++ 3.8memento/src/version1/Memento.java
@@ -0,0 +1,11 @@
+package version1;
+
+public class Memento {
+
+    private String article;
+
+    public Memento(String articleSave) { article = articleSave; }
+
+    public String getSavedArticle() { return article; }
+
+}
